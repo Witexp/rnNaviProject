2d23eda34974a035d4d38ec4d9c569b44a00ee15
Matveev Vitaliy <witexp@rambler.ru>
2020-09-08 14:28:56 +0300

    init and lesson succes 65

---
+++ ReactNativeNavigation/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {AppLoading} from 'expo'
+import { bootstrap } from './src/bootstrap';
+import { AppNavigation } from './src/navigation/AppNavigation'
+
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false)
+  
+  if(!isReady){
+    return <AppLoading 
+              startAsync={bootstrap}
+              onFinish={() => setIsReady(true)}
+              onError = {err => console.log(err)}
+              />
+  }
+  
+  return (
+    <AppNavigation />
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
